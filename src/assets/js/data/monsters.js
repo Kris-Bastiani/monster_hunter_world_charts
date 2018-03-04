2c8a558cb3c6d20db630dc4e08e6e4c215cbeb82
Kris Bastiani <kris.bastiani@gmail.com>
2018-03-04 17:30:51 +1100

    Start assembling JS object of data.

---
+++ src/assets/js/data/monsters.js
@@ -0,0 +1,125 @@
+const monsters = {
+	'Anjanath': {
+		'chron': 6,
+		'weaknesses': {
+			'Fire':      0,
+			'Water':     3,
+			'Thunder':   2,
+			'Ice':       2,
+			'Dragon':    2,
+			'Poison':    1,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      2
+		}
+	},
+	'Azure Rathalos': {
+		'chron': 22
+	},
+	'Barroth': {
+		'chron': 3
+	},
+	'Barroth (Mud)': {
+		'chron': 3
+	},
+	'Bazelgeuse': {
+		'chron': 19
+	},
+	'Black Diablos': {
+		'chron': 23
+	},
+	'Diablos': {
+		'chron': 15
+	},
+	'Dodogama': {
+		'chron': 17
+	},
+	'Great Girros': {
+		'chron': 10
+	},
+	'Great Jagras': {
+		'chron': 0,
+		'weaknesses': {
+			'Fire':      3,
+			'Water':     0,
+			'Thunder':   2,
+			'Ice':       2,
+			'Dragon':    1,
+			'Poison':    3,
+			'Sleep':     3,
+			'Paralysis': 3,
+			'Blast':     3,
+			'Stun':      3
+		}
+	},
+	'Jyuratodus': {
+		'chron': 4
+	},
+	'Jyuratodus (Mud)': {
+		'chron': 4
+	},
+	'Kirin': {
+		'chron': 28
+	},
+	'Kulu-Ya-Ku': {
+		'chron': 1
+	},
+	'Kushala Daora': {
+		'chron': 26
+	},
+	'Lavasioth': {
+		'chron': 20
+	},
+	'Lavasioth (Hard)': {
+		'chron': 20
+	},
+	'Legiana': {
+		'chron': 12
+	},
+	'Nergigante': {
+		'chron': 24
+	},
+	'Odogaron': {
+		'chron': 13
+	},
+	'Paolumu': {
+		'chron': 9
+	},
+	'Pink Rathian': {
+		'chron': 18
+	},
+	'Pukei-Pukei': {
+		'chron': 2
+	},
+	'Radobaan': {
+		'chron': 11
+	},
+	'Rathalos': {
+		'chron': 14
+	},
+	'Rathian': {
+		'chron': 7
+	},
+	'Teostra': {
+		'chron': 25
+	},
+	'Tobi-Kadachi': {
+		'chron': 5
+	},
+	'Tzitzi-Ya-Ku': {
+		'chron': 8
+	},
+	'Uragaan': {
+		'chron': 21
+	},
+	'Vaal Hazak': {
+		'chron': 27
+	},
+	'Xeno\'jiiva': {
+		'chron': 29
+	},
+	'Zorah Magdaros': {
+		'chron': 16
+	}
+}
