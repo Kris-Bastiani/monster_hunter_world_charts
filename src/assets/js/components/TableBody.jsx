13781d9542fdfb3deb10ca64b1489ad8f2e6810f
Kris Bastiani <kris.bastiani@gmail.com>
2018-03-09 08:08:39 +1100

    Create TableBody component.

---
+++ src/assets/js/components/TableBody.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Row from './Row.jsx';
+
+export default class TableBody extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<tbody>
+				{this.props.rows.map(row => {
+					return <Row key={row.name} header={row.name} values={row.weaknesses} />
+				})}
+			</tbody>
+		);
+	}
+}
