aa8363282824da7cdaa994bb4ba8d3ac82464a97
Kris Bastiani <kris.bastiani@gmail.com>
2018-03-28 17:13:49 +1100

    Add remaining monster data.

--- src/assets/js/data/monsters.js
+++ src/assets/js/data/monsters.js
@@ -225,78 +225,306 @@ module.exports = [
 	},
 	{
 		name: 'Kushala Daora',
-		chron: 26
+		chron: 26,
+		weaknesses: {
+			'Fire':      1,
+			'Water':     0,
+			'Thunder':   3,
+			'Ice':       0,
+			'Dragon':    2,
+			'Poison':    3,
+			'Sleep':     1,
+			'Paralysis': 1,
+			'Blast':     3,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Lavasioth',
-		chron: 20
+		chron: 20,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     3,
+			'Thunder':   2,
+			'Ice':       2,
+			'Dragon':    1,
+			'Poison':    3,
+			'Sleep':     1,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Lavasioth (Hard)',
-		chron: 20
+		chron: 20,
+		weaknesses: {
+			'Fire':      1,
+			'Water':     2,
+			'Thunder':   0,
+			'Ice':       0,
+			'Dragon':    1,
+			'Poison':    3,
+			'Sleep':     1,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Legiana',
-		chron: 12
+		chron: 12,
+		weaknesses: {
+			'Fire':      2,
+			'Water':     1,
+			'Thunder':   3,
+			'Ice':       0,
+			'Dragon':    1,
+			'Poison':    3,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Nergigante',
-		chron: 24
+		chron: 24,
+		weaknesses: {
+			'Fire':      1,
+			'Water':     1,
+			'Thunder':   3,
+			'Ice':       1,
+			'Dragon':    2,
+			'Poison':    2,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Odogaron',
-		chron: 13
+		chron: 13,
+		weaknesses: {
+			'Fire':      1,
+			'Water':     1,
+			'Thunder':   2,
+			'Ice':       3,
+			'Dragon':    0,
+			'Poison':    1,
+			'Sleep':     2,
+			'Paralysis': 3,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Paolumu',
-		chron: 9
+		chron: 9,
+		weaknesses: {
+			'Fire':      3,
+			'Water':     0,
+			'Thunder':   2,
+			'Ice':       1,
+			'Dragon':    1,
+			'Poison':    2,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     3,
+			'Stun':      3
+		}
 	},
 	{
 		name: 'Pink Rathian',
-		chron: 18
+		chron: 18,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     1,
+			'Thunder':   2,
+			'Ice':       1,
+			'Dragon':    3,
+			'Poison':    1,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      3
+		}
 	},
 	{
 		name: 'Pukei-Pukei',
-		chron: 2
+		chron: 2,
+		weaknesses: {
+			'Fire':      2,
+			'Water':     0,
+			'Thunder':   3,
+			'Ice':       2,
+			'Dragon':    1,
+			'Poison':    1,
+			'Sleep':     3,
+			'Paralysis': 3,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Radobaan',
-		chron: 11
+		chron: 11,
+		weaknesses: {
+			'Fire':      1,
+			'Water':     1,
+			'Thunder':   1,
+			'Ice':       2,
+			'Dragon':    3,
+			'Poison':    2,
+			'Sleep':     1,
+			'Paralysis': 2,
+			'Blast':     3,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Rathalos',
-		chron: 14
+		chron: 14,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     1,
+			'Thunder':   2,
+			'Ice':       1,
+			'Dragon':    3,
+			'Poison':    1,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Rathian',
-		chron: 7
+		chron: 7,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     1,
+			'Thunder':   2,
+			'Ice':       1,
+			'Dragon':    3,
+			'Poison':    1,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     1,
+			'Stun':      3
+		}
 	},
 	{
 		name: 'Teostra',
-		chron: 25
+		chron: 25,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     3,
+			'Thunder':   1,
+			'Ice':       3,
+			'Dragon':    1,
+			'Poison':    2,
+			'Sleep':     1,
+			'Paralysis': 1,
+			'Blast':     1,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Tobi-Kadachi',
-		chron: 5
+		chron: 5,
+		weaknesses: {
+			'Fire':      2,
+			'Water':     3,
+			'Thunder':   0,
+			'Ice':       2,
+			'Dragon':    1,
+			'Poison':    3,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Tzitzi-Ya-Ku',
-		chron: 8
+		chron: 8,
+		weaknesses: {
+			'Fire':      2,
+			'Water':     2,
+			'Thunder':   3,
+			'Ice':       3,
+			'Dragon':    2,
+			'Poison':    2,
+			'Sleep':     2,
+			'Paralysis': 2,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Uragaan',
-		chron: 21
+		chron: 21,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     3,
+			'Thunder':   1,
+			'Ice':       2,
+			'Dragon':    2,
+			'Poison':    3,
+			'Sleep':     1,
+			'Paralysis': 2,
+			'Blast':     2,
+			'Stun':      3
+		}
 	},
 	{
 		name: 'Vaal Hazak',
-		chron: 27
+		chron: 27,
+		weaknesses: {
+			'Fire':      3,
+			'Water':     0,
+			'Thunder':   1,
+			'Ice':       2,
+			'Dragon':    3,
+			'Poison':    1,
+			'Sleep':     1,
+			'Paralysis': 1,
+			'Blast':     2,
+			'Stun':      2
+		}
 	},
 	{
 		name: 'Xeno\'jiiva',
-		chron: 29
+		chron: 29,
+		weaknesses: {
+			'Fire':      2,
+			'Water':     2,
+			'Thunder':   2,
+			'Ice':       2,
+			'Dragon':    2,
+			'Poison':    3,
+			'Sleep':     0,
+			'Paralysis': 1,
+			'Blast':     2,
+			'Stun':      1
+		}
 	},
 	{
 		name: 'Zorah Magdaros',
-		chron: 16
+		chron: 16,
+		weaknesses: {
+			'Fire':      0,
+			'Water':     3,
+			'Thunder':   1,
+			'Ice':       2,
+			'Dragon':    3,
+			'Poison':    0,
+			'Sleep':     0,
+			'Paralysis': 0,
+			'Blast':     0,
+			'Stun':      0
+		}
 	}
 ];
